394655d1ada5120f09cc146fc6e578f9253095a5
kfamaguana <kfamaguana@gmail.com>
2026-01-22 10:47:23 -0500

    Prueba ERROR corregido

--- math.test.js
+++ math.test.js
@@ -10,7 +10,7 @@ describe('factorial', () => {
   });
 
   test('factorial de 5 debe ser 120', () => {
-    expect(factorial(5)).toBe(100);
+    expect(factorial(5)).toBe(120);
   });
 
   test('factorial de 10 debe ser 3628800', () => {
